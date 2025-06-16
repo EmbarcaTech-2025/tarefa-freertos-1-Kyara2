c2c198c039ec2eb4c8e2b4c512f555dc5c693c09
Kyara-2 <shadoxkan@gmail.com>
2025-06-15 22:42:57 -0300

    codigo clonado do FreeRTOS e compilado com o exemplo do blink funcionamento corretamente

--- main.c
+++ main.c
@@ -5,15 +5,21 @@
 
 #include "hardware/i2c.h"
 
-void led_task() {
-  const uint LED_PIN = 11;
+void led_task(void *params) {
+  const uint LED_PIN = 13;
+
+  // set gpios
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+
   while (true) {
     gpio_put(LED_PIN, 1);
     vTaskDelay(100);
+    // vTaskDelay(pdMS_TO_TICS(500));
+
     gpio_put(LED_PIN, 0);
     vTaskDelay(100);
+    // vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
@@ -23,5 +29,8 @@ int main() {
   xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
   vTaskStartScheduler();
 
-  while(1){};
+  while(1){
+    printf("I really love RTOS!\n");
+    sleep_ms(500);
+  };
 }
